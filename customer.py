e7305698c2c5a654096f3a0a1bc53f5743b8a09e
Nguyễn Trung Kiên <nguyentrungkienbt2023@gmail.com>
2025-11-17 08:11:16 +0700

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,37 +1,57 @@
-from menu_order import see_menu, search_dish, order_dish, list_orders
+from menu_order import see_menu, search_dish, add_dish_cart, view_cart, remove_dish_cart, print_order, confirm_order, list_orders, cart
 def customer_order(customer):
+    customer=input("Nhập Họ và tên: ").strip()
     while True:
         print("\n========== MENU MÓN ĂN ==========")
         print("1. Xem menu")
-        print("2. Tìm kiếm món")
-        print("3. Đặt món")
-        print("4. Xem đơn hàng")
-        print("5. Hủy đơn")
+        print("2. Thêm món vào giỏ hàng ")
+        print("3. Xem giỏ hàng")
+        print("4. Xóa món trong giỏ hàng")
+        print("5. Xác nhận đặt hàng")
+        print("6. Xem tất cả đơn hàng")
         print("0. Thoát!")
-        choose=input("Chọn (0-5): ")
+        choose=input("Chọn (0-6): ").strip()
 
         if choose=='1':
             see_menu()
         elif choose=='2':
-            key=input("Nhập món ăn bạn muốn tìm: ").strip()#tránh khoảng trắng
-            search_dish(key)
+            see_menu()
+            code=input("Nhập mã món: ").strip().upper()
+            dish_name, price=search_dish(code)
+            if dish_name:
+                quantity=int(input("Số lượng: "))
+                note=input("Ghi chú: ")
+                add_dish_cart(dish_name, price, quantity, note)
+            else:
+                print("Mã món không hợp lệ")
         elif choose=='3':
-            order_dish(customer)
+                view_cart()
         elif choose=='4':
-            for i in list_orders:
-                if (i["customer"]==customer):
-                    print(i)
+            view_cart()
+            index=input("Nhập số thứ tự món cần xóa: ").strip()
+            if index.isdigit():
+                idx=int(index)-1
+                remove_dish_cart(idx)
         elif choose=='5':
-            name=customer
-            for i in list_orders:
-                if i["customer"]==name and i["status"]=="Mới đặt":
-                    list_orders.remove(i)
-                    print("Hủy đơn thành công")
-                    break
-                else:
-                    print("Hủy đơn không thành công!")
+            if not cart:
+                print("Giỏ hàng trống, vui lòng thêm món vào trước khi đặt hàng")
+                continue
+            delivery=input("Hình thức (tại chỗ/mang đi): ").lower().strip()
+            if delivery not in ['tại chỗ', 'mang đi']:
+                print("Hình thức không hợp lệ, mặc định là 'mang đi")
+                delivery='mang đi'
+            confirm_order(customer, delivery)
+        elif choose=='6':
+            if not list_orders:
+                print("Chưa có đơn hàng nào")
+            else:
+                for i in list_orders:
+                    print_order(i)
         else:
             break
-print(customer_order("nguyễn trung kiên"))
+#customer_order("nguyễn trung kiên")
+
+
+
 
 
